67c05f02a88e15d3de1d2fec77a235b6a8043063
MOHIT SINGH <2021pcs1020@iitjammu.ac.in>
2022-08-04 02:31:59 +0530

    Time: 3 ms (67.01%), Space: 5.9 MB (52.17%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        vector<uint32_t> r(32,0);
-        uint32_t sum=0,t=4294967296;
-        int i=0;
-        string s="";
+        uint32_t result = 0;
         
-        for(int i=0;i<32;i++){
-           r[i]=n&1;
-            // cout<<r[i];
-           s=s+to_string(r[i]);
-            n=n>>1;
-           
-            
-       }
-       // cout<<endl<<"s is: "<<s;
-        // while(i<32){
-        //     s=s+r[i];
-        //     i++;
-        // }
-           char* end;
-        return strtoul(s.c_str(), &end, 2);
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            result |= n & 1;
+            n >>= 1;
+        }
+        
+        return result;
     }
 };
