eeaf489f703ac71d377502bf1e28c6faa937b7bf
MOHIT SINGH <2021pcs1020@iitjammu.ac.in>
2022-08-05 02:32:58 +0530

    Time: 1 ms (66.92%), Space: 5.7 MB (94.33%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isHappy(int n) {
         int sum=0,count=0;
-        while(count<500){
+        while(count<50){
         while(n!=0){
             sum=sum+((n%10)*(n%10));
             // cout<<"sum of: "<<n%10<<"="<<sum<<endl;
