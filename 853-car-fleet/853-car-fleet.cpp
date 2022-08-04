d2494acab8fb7a3de832900b48a24e0f76d18841
MOHIT SINGH <2021pcs1020@iitjammu.ac.in>
2022-08-04 16:01:41 +0530

    Time: 1300 ms (5.02%), Space: 78.7 MB (50.29%) - LeetHub

---
+++ 853-car-fleet/853-car-fleet.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        stack<float>s;
+        float t,count=0;
+        vector<pair<int,int>>final;
+        for(int i=0;i<position.size();i++){
+            final.push_back({position[i],speed[i]});
+        }
+        sort(final.begin(),final.end());
+        
+        
+        
+        for(int i=speed.size()-1;i>=0;i--){
+            t=(target-final[i].first)/float(final[i].second);
+            cout<<t<<endl;
+            if(s.empty() || s.top()<t){
+                s.push(t);
+                 cout<<"for push: "<<t<<endl;
+                count++;
+            }
+        }
+        return count;
+    }
+};
