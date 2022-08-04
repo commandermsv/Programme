605c9390df7a3064ee54b866079ef2f5cd01658b
MOHIT SINGH <2021pcs1020@iitjammu.ac.in>
2022-08-05 03:06:19 +0530

    Time: 3 ms (65.23%), Space: 8.8 MB (12.41%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int temp=0;
-        for(int i=digits.size()-1;i>=0;i--){
-            
-            if(i==digits.size()-1) temp=digits[i]+1;
-            else temp=digits[i]+temp;
-            
-            
-            if(temp==10){
-                if(i==0){
-                    digits[0]=0;
-                     digits.insert(digits.begin(),1);
-                    return digits;
-                }
-                digits[i]=0;
-                temp=1;                
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
             }
-            else {digits[i]=temp;temp=0;}
-            if(i==0 && temp>0 && digits[0]==9){
-                digits[0]=0;
-                digits.insert(digits.begin(),1);
-                break;
-            }
+            digits[i] = 0;
         }
+        
+        digits[0] = 1;
+        digits.push_back(0);
         return digits;
     }
 };
