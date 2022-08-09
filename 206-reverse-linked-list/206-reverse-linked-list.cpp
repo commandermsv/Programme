e14b8465d05770b1b69b9ae2853c15183128cb91
MOHIT SINGH <2021pcs1020@iitjammu.ac.in>
2022-08-09 23:55:58 +0530

    Time: 10 ms (54.63%), Space: 8.6 MB (10.40%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -13,7 +13,7 @@ public:
     ListNode* reverseList(ListNode* head) {
         if (head == NULL || head->next == NULL)
             return head;
-        // Recursive call
+     
         ListNode* rest = reverseList(head->next);
         head->next->next = head;
         
