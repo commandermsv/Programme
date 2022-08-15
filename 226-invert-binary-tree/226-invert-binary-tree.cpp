21b64f25d9e9ed7633dc8de037996f82c9f74968
MOHIT SINGH <2021pcs1020@iitjammu.ac.in>
2022-08-16 01:01:11 +0530

    Time: 3 ms (70.96%), Space: 9.7 MB (38.50%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -14,9 +14,8 @@ public:
     TreeNode* invertTree(TreeNode* root) {
         if(root==NULL) return root;
         swap(root->left,root->right);
-        invertTree(root->right);
         invertTree(root->left);
-        
+        invertTree(root->right);
         
         return root;
         
