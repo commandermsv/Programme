959b630a256cf21ba78970e76703a26ad8ca1730
MOHIT SINGH <2021pcs1020@iitjammu.ac.in>
2022-08-27 02:08:51 +0530

    Time: 132 ms (28.24%), Space: 47.3 MB (9.41%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,42 +1,13 @@
 class Solution {
-private:
-    priority_queue<int> pq;
-    map<int,int> m;
-    
 public:
-    int j=0,result;
     int findKthLargest(vector<int>& nums, int k) {
-        // int first=nums[0];
-        // pq.push(nums[0]);
-        for(int i=0;i<nums.size();i++){
-                pq.push(nums[i]);
-            
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int i = 0; i < nums.size(); i++) {
+            pq.push(nums[i]);
+            if (pq.size() > k) {
+                pq.pop();
+            }
         }
-        while(j<k){
-            result=pq.top();
-            // cout<<result<<endl;
-            pq.pop();
-            j++;
-        }
-        return result;
-        
-        
-        // by hashmap
-//         int j=0;
-//          int s=nums.size();
-    
-//         for(int i=0;i<s;i++){
-//             if(m.find(nums[i])==m.end()) {
-//                 m[nums[i]]++;
-//                 cout<<nums[i]<<endl;
-//             }
-//         }    
-//         for(auto i=m.rbegin();i!=m.rend();i++){
-//             if(j<k)
-//              result=i->first;
-//             else break;
-//             j++;
-//         }
-//     return result;
+        return pq.top();
     }
 };
