a67b5935cb42265359cf713aac44d3a25621dc0a
MOHIT SINGH <2021pcs1020@iitjammu.ac.in>
2022-08-27 01:05:02 +0530

    Time: 603 ms (23.19%), Space: 73.6 MB (33.88%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,7 +1,9 @@
 class Solution {
+    private:
+            priority_queue<pair<double, vector<int>>> pq;
+
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<double, vector<int>>> pq;
         for (int i = 0; i < points.size(); i++) {
             double distance = sqrt(pow(points[i][0], 2) + pow(points[i][1], 2));
             pq.push({distance, points[i]});
