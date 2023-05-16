0f0cf00104e2949af350a10ea8d8918eeb44c74f
MOHIT SINGH <2021pcs1020@iitjammu.ac.in>
2023-05-16 12:59:23 +0530

    Time: 9 ms (70.90%), Space: 7.2 MB (83.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,8 +3,8 @@ public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false;
         
-        map<char,int> m1;
-        map<char,int> m2;
+        unordered_map<char,int> m1;
+        unordered_map<char,int> m2;
         for(int i=0;i<s.size();i++){
             m1[s[i]]++;
             m2[t[i]]++;
